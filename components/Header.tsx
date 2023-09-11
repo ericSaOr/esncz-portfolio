e8f6c8600e89024f37e24d45a968ee900bd2a4e6
Eric Sanchez <ericfsod@gmail.com>
2023-09-11 13:14:48 -0400

    Finished header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <header className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20'> {/*this sticks the header to the top*/}
-        <div className='flex flex-row items-center'>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'> {/*this sticks the header to the top*/}
+        <motion.div initial= {{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className='flex flex-row items-center'>
         {/*social icons */}
         <SocialIcon 
         url="https:/www.github.com/ericSaOr" 
@@ -22,9 +36,21 @@ const Header = (props: Props) => {
         fgColor="gray" 
         bgColor="transparent" 
         />
-        </div>
-
-        <div className='flex flex-row items-center text-gray-300 cursor pointer'>
+        </motion.div>
+        
+        <motion.div
+          initial={{
+            x:500,
+            opacity:0,
+            scale:0.5,
+          }} 
+          animate={{
+            x:0,
+            opacity:1,
+            scale:1,
+          }}
+          transition={{duration: 1}}
+        className='flex flex-row items-center text-gray-300 cursor pointer'>
             <SocialIcon 
             className='cursor-pointer'
             network='email'
@@ -33,7 +59,7 @@ const Header = (props: Props) => {
             
             />
             <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Contact Me!</p>
-        </div>
+        </motion.div>
     </header>
     //changed to <header> for SEO 
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,9 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Hero */}
-
+      <section id="hero">
+        <Hero />
+      </section>
       {/* About */}
 
       {/* Experience */}
