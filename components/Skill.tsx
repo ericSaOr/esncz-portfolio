6e160b002978b175496997ac9d2aa946770b68b0
Eric Sanchez <ericfsod@gmail.com>
2023-10-24 20:20:40 -0400

    added the placeholders for the individual skill elements more css.

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
-type Props = {}
+type Props = {
+    directionleft?: boolean;
+}
 
-const Skill = (props: Props) => {
+const Skill = ({directionleft}: Props) => {
   return (
-    <div>Skill</div>
+    <div className='group relative flex cursor-pointer'>
+        <motion.img 
+        initial={{
+            x: directionleft ? -200:200,
+            opacity: 0
+        }}
+        transition={{duration: 1}}
+        whileInView={{opacity: 1, x: 0}}
+        src="img.jpg"
+        className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale
+        transition duration-300 ease-in-out'
+        />
+    </div>
   )
 }
 
