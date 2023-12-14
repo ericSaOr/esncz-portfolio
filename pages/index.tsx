b2e9b692d68d2e9cc95ce207885519dc23d34795
Eric Sanchez <ericfsod@gmail.com>
2023-12-14 13:56:06 -0500

    added percentage to skills section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Experience from '@/components/Experience'
 import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +39,9 @@ export default function Home() {
       </section>
 
       { /* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       { /* Contact Me */}
 
